75a7c396929ecaf2f52e0f821f9c68424e8b1430
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-20 15:06:19 -0300

    feat: implementing parcelable in pet

--- app/src/main/java/com/macena/petlife/model/Pet.kt
+++ app/src/main/java/com/macena/petlife/model/Pet.kt
@@ -1,5 +1,8 @@
 package com.macena.petlife.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Pet(
     val name: String,
     val birthDate: String,
@@ -9,4 +12,40 @@ data class Pet(
     val lastVetVisit: String,
     val lastVaccination: String,
     val lastPetShopVisit: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(birthDate)
+        parcel.writeString(type)
+        parcel.writeString(color)
+        parcel.writeString(size)
+        parcel.writeString(lastVetVisit)
+        parcel.writeString(lastVaccination)
+        parcel.writeString(lastPetShopVisit)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Pet> {
+        override fun createFromParcel(parcel: Parcel): Pet {
+            return Pet(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Pet?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
