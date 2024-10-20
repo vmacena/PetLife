4d06c0102f2df9919d1e86e7045b5703635e61a4
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-20 14:18:27 -0300

    feat: instantiating standard pet

--- app/src/main/java/com/macena/petlife/MainActivity.kt
+++ app/src/main/java/com/macena/petlife/MainActivity.kt
@@ -6,13 +6,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.macena.petlife.model.Pet
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var pet: Pet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        pet = Pet(
+            "Bob",
+            "08/03/2021",
+            "Dog",
+            "Gray",
+            "Big",
+            "05/06/2024",
+            "05/06/2024",
+            "18/09/2024",
+        )
 
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
