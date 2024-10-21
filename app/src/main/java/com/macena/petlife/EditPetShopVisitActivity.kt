09f801ccebf3335703bd373f9af5b0f09439dff9
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-10-20 23:10:27 -0300

    feat: creating EditPetShopVisitActivity

---
+++ app/src/main/java/com/macena/petlife/EditPetShopVisitActivity.kt
@@ -0,0 +1,33 @@
+package com.macena.petlife
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class EditPetShopVisitActivity : AppCompatActivity() {
+
+    private lateinit var editPetShopVisitDate: EditText
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_pet_shop_visit)
+
+        editPetShopVisitDate = findViewById(R.id.editPetShopVisitDate)
+        val saveButton: Button = findViewById(R.id.savePetShopVisitButton)
+
+        val currentPetShopVisitDate = intent.getStringExtra("lastPetShopVisit")
+        editPetShopVisitDate.setText(currentPetShopVisitDate)
+
+        saveButton.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("lastPetShopVisit", editPetShopVisitDate.text.toString())
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
