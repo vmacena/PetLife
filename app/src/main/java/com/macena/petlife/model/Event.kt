31a24a996b10b530d1757b11aede62f7e768a2c7
vmacena <macena.v@aluno.ifsp.edu.br>
2024-12-08 18:58:51 -0300

    refactor: removing old models that were used in project refactoring

--- app/src/main/java/com/macena/petlife/model/Event.kt
+++
@@ -1,3 +0,0 @@
-package com.macena.petlife.model
-
-data class Event(val id: Int, val type: String, val date: String)
--- app/src/main/java/com/macena/petlife/model/Pet.kt
+++
@@ -1,3 +0,0 @@
-package com.macena.petlife.model
-
-data class Pet(val id: Int, val name: String, val type: String)
